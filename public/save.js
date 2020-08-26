dc84378fa85c413a7f43cbc91a999f3d6ccab56a
ijimaso <soxup0708@gmail.com>
2020-08-26 15:29:12 +0900

    add saving

---
+++ public/save.js
@@ -0,0 +1,7 @@
+// クローリング → 収集結果保存(ユーザ用)
+const saveButton = document.getElementById('save-button');
+saveButton.addEventListener('click', (e) => {
+    // e.preventDefault();
+    console.log('Hello, save.js!!');
+    // const blob = new Blob()
+})
