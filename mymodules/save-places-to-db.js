cb12982849b0af5060791f225c1f4134200621c6
ijimaso <soxup0708@gmail.com>
2020-09-23 11:18:24 +0900

    update

--- mymodules/save-places-to-db.js
+++ mymodules/save-places-to-db.js
@@ -1,10 +1,6 @@
 const mongoClient = require('mongodb').MongoClient;
 
 const savePlacesToDb = async (parameter, places) => {
-    // 重複データは排除する
-    const uniquePlaces = places.filter((element, index, self) =>
-        self.findIndex(e => e['place_id'] === element['place_id']) === index);
-
     // MongoDB設定
     const dbUrl = 'mongodb://localhost:27017';
     const connectOption = {
@@ -19,16 +15,26 @@ const savePlacesToDb = async (parameter, places) => {
             console.log(err);
         }
 
-        console.log("Connected successfully to server");
         const db = client.db(dbName);
         const collection = await db.createCollection(parameter['area-name']);
 
-        // TODO: 既にコレクションが存在する場合の例外処理
-        // TODO: コレクションが存在する場合は，データを上書きする
+        // _idにplace_idを挿入してから，DBにinsert
+        let dupNumber = 0;
+        for (const place of places) {
+            place['_id'] = place['place_id'];
 
-        await collection.insertMany(uniquePlaces);
+            try {
+                await collection.insertOne(place);
+            } catch (err) {
+                if (err.code == '11000') {
+                    dupNumber += 1;
+                }
+            }
+        }
 
-        console.log('Saved unique places to MongoDB');
+        console.log(`Total ${places.length} places`);
+        console.log(`Saved ${places.length - dupNumber} places to MongoDB.`);
+        console.log(`${dupNumber} places are duplicated.`);
         console.log(`DB Name: ${dbName}`);
         console.log(`Collection Name: ${parameter['area-name']}`);
         client.close();
